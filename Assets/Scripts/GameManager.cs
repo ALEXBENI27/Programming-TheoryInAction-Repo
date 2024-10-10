2427a238ed2d60e02f7313fc99c413f679db09b7
Sergio Carrillo <segio_beni27@hotmail.com>
2024-10-10 12:33:56 -0600

    Fourth commit
    I added some script to control player and vehicles and a script to spawn vehicles automatically

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,26 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public TMP_InputField playerName;
+    public TMP_InputField inputName;
+    public string playerName;
+    public static GameManager Instance;
+
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(gameObject);
+            return;
+        }    
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayerName(string name) {
+        GameManager.Instance.playerName = name;
+    }
 
     public void StartGame(){
+        PlayerName(inputName.text);
         SceneManager.LoadScene(1);
     }
 
